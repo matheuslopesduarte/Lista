b41e550d27a7551791535460dd244231b1f0dc05
matheuslopesduarte <matheuslopesduarte7@gmail.com>
2023-12-06 08:55:02 -0300

    aaaaaaaaaaaaaaaaaaaaaa

--- main.js
+++ main.js
@@ -6,12 +6,23 @@
     let finalizadas = [];
 
     function NewTarefa() {
-        let descricao = document.getElementById('descricao').value;
+        let descricao = document.getElementById('descricao').value.trim();
         if (descricao == '') {
             alert('A descrição não pode estar vazia');
         } else {
+            if(descricao.length > 75){
+                alert(`O maximo de caracteres é 75 (${descricao.length - 75} acima)`);
+            } else {
             itens.push(descricao);
-            descricao = '';
+            document.getElementById('descricao').value = '';
+            AtualizaTabelas();
+            }
+        }
+        
+    }
+    function DeleteTodos(){
+        if(confirm('Tem certeza que deseja excluir todos as tarefas finalizadas?')){
+            finalizadas = [];
             AtualizaTabelas();
         }
     }
@@ -21,16 +32,21 @@
         AtualizaTabelas();
     }
     function RemoveFinalizada(id){
+        if(confirm('Deseja realmente excluir esta tarefa finalizada?')){
         finalizadas.splice(id, 1);
         AtualizaTabelas();
+        }
     }
     function AtualizaTabelas() {
         let r1 = '';
         let r2 = '';
+        if(finalizadas.length > 1){  
+        r2 = '<button id="delete-todos" onclick="DeleteTodos()">Excluir Todos</button>';
+        }
         for (let index = 0; index < itens.length; index++) {
-            r1 += `<li>${itens[index]} <button onclick='RemoveTarefa(${index})'>Concluir</button></li>`;}
+            r1 += `<li>${itens[index]}&nbsp;<button onclick='RemoveTarefa(${index})'>Concluir</button></li>`;}
         for (let index = 0; index < finalizadas.length; index++) {
-            r2 += `<li>${finalizadas[index]} <button onclick='RemoveFinalizada(${index})'>Excluir</button></li>`;}
+            r2 += `<li>${finalizadas[index]}&nbsp;<button onclick='RemoveFinalizada(${index})'>Excluir</button></li>`;}
         tarefasDiv.innerHTML = r1;
         finalizadasDiv.innerHTML = r2;   
         tarefasT.innerHTML = `Tarefas Pendentes (${itens.length})`;
